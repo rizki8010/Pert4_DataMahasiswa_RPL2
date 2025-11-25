22d0acca035afa6f08c0a7011d769820ec6d21e1
rizki ilham <rizkiilhammuria@gmail.com>
2025-11-25 16:23:32 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert4_51422467/Pert4_51422467.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert4_51422467;
+
+/**
+ *
+ * @author Asus
+ */
+public class Pert4_51422467 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/pert4_51422467/controller/MahasiswaController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert4_51422467.controller;
+
+import com.mycompany.pert4_51422467.model.ModelMahasiswa;
+import java.util.List;
+
+/**
+ *
+ * @author Asus
+ */
+public interface MahasiswaController {
+    public void addMhs(ModelMahasiswa mhs);
+    public ModelMahasiswa getMhs(int id);
+    public void updateMhs(ModelMahasiswa mhs);
+    public void deleteMhs(int id);
+    public List<ModelMahasiswa> getAllMahasiswa();
+}
